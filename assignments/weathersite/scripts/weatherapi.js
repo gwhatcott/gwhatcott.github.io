32f01cc8dcc390265f1466c2fa56405bdb0ea865
gwhatcott <38670593+gwhatcott@users.noreply.github.com>
2018-06-28 22:17:34 -0600

    1
    123

---
+++ assignments/weathersite/scripts/weatherapi.js
@@ -0,0 +1,9 @@
+ var weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET','https://api.openweathermap.org/data/2.5/forecast?id=4156210&APPID=deeffff5d2fa7e2da27e48104defb89a',   true);
+
+weatherRequest.responseType = 'text';
+weatherRequest.send();
+weatherRequest.onload = function() {
+    var weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+} //end of the function
